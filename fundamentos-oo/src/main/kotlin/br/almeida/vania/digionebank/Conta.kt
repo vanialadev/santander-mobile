29cf23135598aa1e2941fe66254a821a9438fcbc
Vania Almeida <vania.almeida28@hotmail.com>
2021-07-02 00:19:32 -0300

    classe conta

---
+++ fundamentos-oo/src/main/kotlin/br/almeida/vania/digionebank/Conta.kt
@@ -0,0 +1,17 @@
+package br.almeida.vania.digionebank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+
+    fun deposita(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
