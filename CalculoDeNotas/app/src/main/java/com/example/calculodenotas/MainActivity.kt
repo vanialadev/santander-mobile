cfa1439b3929bddd643e3d9b293c583272103cb7
Vania Almeida <vania.almeida28@hotmail.com>
2021-07-06 11:28:26 -0300

    projeto iniciado

---
+++ CalculoDeNotas/app/src/main/java/com/example/calculodenotas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calculodenotas
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculodenotas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
