639b6488aa9c32f240fac812cac84638a7de0db1
Vania Almeida <vania.almeida28@hotmail.com>
2021-07-05 21:12:41 -0300

    companion object

---
+++ fundamentos-oo/src/main/kotlin/br/almeida/vania/digionebank/ImprimeRelatorioFuncionario.kt
@@ -0,0 +1,12 @@
+package br.almeida.vania.digionebank
+
+class ImprimeRelatorioFuncionario {
+
+    companion object {
+        fun imprime(funcionario: Funcionario) {
+            println(
+                funcionario.toString()
+            )
+        }
+    }
+}
