cd4b085bd47b7f8b6cbfd6a4a2da382c7e9863fd
Vania Almeida <vania.almeida28@hotmail.com>
2021-07-06 20:07:44 -0300

    projeto criado

---
+++ collections-arrays-lists/src/main/kotlin/br/almeida/vania/collections/Hello.kt
@@ -0,0 +1,6 @@
+package br.almeida.vania.collections
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ collections-arrays-lists/src/test/kotlin/br/almeida/vania/collections/HelloTest.kt
@@ -0,0 +1,8 @@
+package br.almeida.vania.collections
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
