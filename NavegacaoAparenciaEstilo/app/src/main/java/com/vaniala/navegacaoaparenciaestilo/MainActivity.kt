bf890e503c06f8409baf9c982dd490967a741aba
Vania Almeida <vania.almeida28@hotmail.com>
2021-07-31 03:04:26 -0300

    menu finalizado

--- NavegacaoAparenciaEstilo/app/src/main/java/com/vaniala/navegacaoaparenciaestilo/MainActivity.kt
+++ NavegacaoAparenciaEstilo/app/src/main/java/com/vaniala/navegacaoaparenciaestilo/MainActivity.kt
@@ -2,15 +2,19 @@ package com.vaniala.navegacaoaparenciaestilo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private val rvList: RecyclerView by lazy {
-        findViewById<RecyclerView>(R.id.rv_list)
+        findViewById(R.id.rv_list)
     }
 
     private val adapter = ContactAdapter()
@@ -29,22 +33,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateList() {
-        adapter.updateList(arrayListOf(
-            Contact(
-                name = "vania",
-                phone = "(85) 98888-8888",
-                photograph = "img.png"
-            ),
-            Contact(
-                name = "erica",
-                phone = "(85) 98888-8888",
-                photograph = "img.png"
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    name = "vania",
+                    phone = "(85) 98888-8888",
+                    photograph = "img.png"
+                ),
+                Contact(
+                    name = "erica",
+                    phone = "(85) 98888-8888",
+                    photograph = "img.png"
+                )
             )
-        ))
+        )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
