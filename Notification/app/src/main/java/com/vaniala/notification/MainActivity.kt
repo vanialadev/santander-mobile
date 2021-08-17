528b922fa84291aa89e24bd5be25198001d235f2
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 08:31:12 -0300

    app notification

---
+++ Notification/app/src/main/java/com/vaniala/notification/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vaniala.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
