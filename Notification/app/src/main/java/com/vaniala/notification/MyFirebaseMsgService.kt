11d197599a9250edae107e92e6f289ae73e48c35
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 12:31:28 -0300

    icone notificacao

--- Notification/app/src/main/java/com/vaniala/notification/MyFirebaseMsgService.kt
+++ Notification/app/src/main/java/com/vaniala/notification/MyFirebaseMsgService.kt
@@ -4,7 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 
-class MyFirebaseInstanceIdService : Service() {
+class MyFirebaseMsgService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
