cd49f794bc80611476b00b00a15a35d13920b4aa
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 08:34:33 -0300

    button

--- Notification/app/src/main/java/com/vaniala/notification/MainActivity.kt
+++ Notification/app/src/main/java/com/vaniala/notification/MainActivity.kt
@@ -6,8 +6,13 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {  }
     }
 }
