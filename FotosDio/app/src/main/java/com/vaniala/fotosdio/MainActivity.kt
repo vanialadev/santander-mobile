54a50dc18d05ba9072c5f5efe5487c3c5f5fd4f2
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 15:45:32 -0300

    acesso a biblioteca de imagens

--- FotosDio/app/src/main/java/com/vaniala/fotosdio/MainActivity.kt
+++ FotosDio/app/src/main/java/com/vaniala/fotosdio/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.vaniala.fotosdio
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPicker = findViewById<Button>(R.id.pick_button)
+        img = findViewById(R.id.imageview_image)
+
+        btnPicker.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        REQUEST_PERMISSION
+                    )
+                } else {
+                    pickImagefromGalary()
+
+                }
+            } else {
+                pickImagefromGalary()
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImagefromGalary()
+                }else {
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun pickImagefromGalary() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+
+            img.setImageURI(data?.data)
+        }
+    }
+
+    companion object {
+        private val REQUEST_PERMISSION = 1
+        private val IMAGE_PICK_CODE = 2
+
+
     }
 }
