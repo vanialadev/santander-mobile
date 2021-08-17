4b9795937af3616103ef5542601738df679fa19a
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 12:33:37 -0300

    corrigindo a criação da mensagem

--- Notification/app/src/main/java/com/vaniala/notification/NotificationUtils.kt
+++ Notification/app/src/main/java/com/vaniala/notification/NotificationUtils.kt
@@ -14,34 +14,40 @@ import androidx.core.app.NotificationCompat
 
 private lateinit var notificationChannel: NotificationChannel
 private lateinit var notificationManager: NotificationManager
+
 @SuppressLint("StaticFieldLeak")
 private lateinit var builder: NotificationCompat.Builder
 
 
 fun Context.showNotification(channelId: String, title: String, body: String) {
-    notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     val intent = Intent(this, MainActivity::class.java)
-    val pendingIntent =
-        PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    val pendingIntent = PendingIntent.getActivity(
+        this, 0, intent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val channelId = getString(R.string.default_notification_channel_id)
+    val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+    val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+        .setContentTitle(title)
+        .setContentText(body)
+        .setAutoCancel(true)
+        .setSound(defaultSoundUri)
+        .setContentIntent(pendingIntent)
+
+    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        notificationChannel =
-            NotificationChannel(channelId, body, NotificationManager.IMPORTANCE_HIGH).apply {
-                lightColor = Color.BLUE
-                enableVibration(true)
-            }
-        notificationManager.createNotificationChannel(notificationChannel)
-
-        builder = NotificationCompat.Builder(this, channelId).apply {
-            setSmallIcon(R.drawable.ic_refresh)
-            setContentTitle(title)
-            setContentText(body)
-            setAutoCancel(true)
-            setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
-            setContentIntent(pendingIntent)
-        }
+        val channel = NotificationChannel(
+            channelId,
+            "Channel human readable title",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 
-    notificationManager.notify(channelId.toInt(), builder.build())
+    notificationManager.notify(0 , notificationBuilder.build())
 }
