a09a3d8b689e8ef82a1d8d766d5b93637a1f7cda
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 12:32:24 -0300

    service pra mostrar a msg e no manifest configurar o icone cor e channel

--- Notification/app/src/main/java/com/vaniala/notification/MyFirebaseMsgService.kt
+++ Notification/app/src/main/java/com/vaniala/notification/MyFirebaseMsgService.kt
@@ -3,10 +3,44 @@ package com.vaniala.notification
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMsgService : Service() {
+class MyFirebaseMsgService : FirebaseMessagingService() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            Log.d(TAG, "Message data payload: ${remoteMessage.notification}")
+
+            if (remoteMessage.notification != null) {
+                this.showNotification(
+                    "1234",
+                    remoteMessage.notification?.title.toString(),
+                    remoteMessage.notification?.body.toString()
+                )
+            } else {
+
+            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            this.showNotification(
+                "1234",
+                remoteMessage.notification?.title.toString(),
+                remoteMessage.notification?.body.toString()
+            )
+        }
+    }
+
+    companion object {
+
+        private const val TAG = "MyFirebaseMsgService"
     }
 }
