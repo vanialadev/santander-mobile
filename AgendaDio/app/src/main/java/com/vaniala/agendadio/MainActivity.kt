ee623fd0f9fda5fcbccff52c180ca0165b018864
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 14:35:42 -0300

    intent pra abrir uma agenda

--- AgendaDio/app/src/main/java/com/vaniala/agendadio/MainActivity.kt
+++ AgendaDio/app/src/main/java/com/vaniala/agendadio/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.vaniala.agendadio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSrtEvent = findViewById<Button>(R.id.set_event)
+
+        btnSrtEvent.setOnClickListener {
+
+            val intent =  Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "bootcamp santander")
+                .putExtra(EVENT_LOCATION, "on line")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, 60 * 60 * 1000 + System.currentTimeMillis())
+
+            startActivity(intent)
+        }
     }
 }
