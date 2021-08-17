22e8e198986d19868d40764e21b42ab0cceb40b3
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-16 21:19:18 -0300

    pegando o provider

--- ClienteProvider/app/src/main/java/com/vaniala/clienteprovider/MainActivity.kt
+++ ClienteProvider/app/src/main/java/com/vaniala/clienteprovider/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.vaniala.clienteprovider
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -15,9 +18,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         noteRecycler = findViewById(R.id.client_list)
         noteRefreshButton = findViewById(R.id.client_button_refresh)
+        getContentProvider()
+
         noteRefreshButton.setOnClickListener {
+            getContentProvider()
+        }
+
+    }
+
+    private fun getContentProvider() {
+        try {
+            val url = "content://com.vania.notesprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(
+                    data, null, null, null, "title"
+                )
+            noteRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
