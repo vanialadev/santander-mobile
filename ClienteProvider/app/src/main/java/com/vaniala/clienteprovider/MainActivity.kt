e1927eb46d3bdf06a2653b8ebb2884bb190c146c
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-16 21:18:51 -0300

    main activity

--- ClienteProvider/app/src/main/java/com/vaniala/clienteprovider/MainActivity.kt
+++ ClienteProvider/app/src/main/java/com/vaniala/clienteprovider/MainActivity.kt
@@ -2,10 +2,22 @@ package com.vaniala.clienteprovider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteRecycler: RecyclerView
+    private lateinit var noteRefreshButton: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        noteRecycler = findViewById(R.id.client_list)
+        noteRefreshButton = findViewById(R.id.client_button_refresh)
+        noteRefreshButton.setOnClickListener {
+        }
     }
 }
