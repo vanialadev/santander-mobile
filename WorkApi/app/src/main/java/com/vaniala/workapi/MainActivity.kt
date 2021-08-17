a238b0b0321a84482325f816a0d2f51db13f8557
Vania Almeida <vania.almeida28@hotmail.com>
2021-08-17 13:44:51 -0300

    adapter

--- WorkApi/app/src/main/java/com/vaniala/workapi/MainActivity.kt
+++ WorkApi/app/src/main/java/com/vaniala/workapi/MainActivity.kt
@@ -2,10 +2,16 @@ package com.vaniala.workapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerProducts: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerProducts = findViewById(R.id.recycler_products)
     }
 }
---
+++ WorkApi/app/src/main/java/com/vaniala/workapi/ProductAdapter.kt
@@ -0,0 +1,4 @@
+package com.vaniala.workapi
+
+class ProductAdapter {
+}
