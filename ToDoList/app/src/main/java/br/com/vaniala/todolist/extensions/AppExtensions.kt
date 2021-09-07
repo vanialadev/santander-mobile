276863d56a91d693a5d0e56b105815aaf73640fc
Vania Almeida <vania.almeida28@hotmail.com>
2021-09-07 19:46:51 -0300

    criacao da nova tela e do data picker e extensions

--- ToDoList/app/src/main/java/br/com/vaniala/todolist/extensions/AppExtensions.kt
+++ ToDoList/app/src/main/java/br/com/vaniala/todolist/extensions/AppExtensions.kt
@@ -1,4 +1,17 @@
 package br.com.vaniala.todolist.extensions
 
-class AppExtensions {
+import com.google.android.material.textfield.TextInputLayout
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val locale = Locale("pt", "BR")
+
+fun Date.format(): String {
+    return SimpleDateFormat("dd/MM/yyyy", locale).format(this)
 }
+
+var TextInputLayout.text: String
+    get() = editText?.text?.toString() ?: ""
+    set(value) {
+        editText?.setText(value)
+    }
--- ToDoList/app/src/main/java/br/com/vaniala/todolist/ui/AddTaskActivity.kt
+++ ToDoList/app/src/main/java/br/com/vaniala/todolist/ui/AddTaskActivity.kt
@@ -1,4 +1,33 @@
 package br.com.vaniala.todolist.ui
 
-class AddTaskActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.vaniala.todolist.databinding.ActivityAddTaskBinding
+import br.com.vaniala.todolist.extensions.format
+import br.com.vaniala.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+    private lateinit var binding:  ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            var datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+               binding.tilDate.text = Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+    }
 }
