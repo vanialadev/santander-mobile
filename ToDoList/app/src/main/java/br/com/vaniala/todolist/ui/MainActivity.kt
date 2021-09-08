80678d6dff80331fcc9fe724afa51b49099edffe
Vania Almeida <vania.almeida28@hotmail.com>
2021-09-07 21:28:47 -0300

    listando as tasks

--- ToDoList/app/src/main/java/br/com/vaniala/todolist/ui/MainActivity.kt
+++ ToDoList/app/src/main/java/br/com/vaniala/todolist/ui/MainActivity.kt
@@ -1,13 +1,52 @@
 package br.com.vaniala.todolist.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import br.com.vaniala.todolist.databinding.ActivityMainBinding
+import br.com.vaniala.todolist.datasource.TaskDataSource
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter()}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+
+        binding.btnFab.setOnClickListener {
+            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+        }
+
+        adapter.listenerEdit = {
+
+            Log.e("Tag", "listener $it")
+        }
+
+        adapter.listenerDelete = {
+            Log.e("Tag", "listener $it")
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CREATE_NEW_TASK){
+            binding.rvTasks.adapter = adapter
+            adapter.submitList(TaskDataSource.getList())
+        }
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
     }
 }
